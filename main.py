e37b9ff4d2b5dc29728f341bfc04bc564211f721
YazRaso <daedricrend@gmail.com>
2024-12-22 12:44:45 +0400

    Adjusted volatility to take into account time period

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import pandas as pd
 import numpy as np
 import math
 
+from scipy.ndimage import maximum
+
 # Use Kaggle dataset
 stock_data = pd.read_csv('./GOOG.csv')
 
 # Initialize dataframe
 df = pd.DataFrame(stock_data)
-
-# TODO: Clean data to represent 252 trading days in a year
-df.dropna()
-df.drop_duplicates()
+df = df.drop_duplicates()
 
 # Calculate Daily returns
 df['Daily Returns'] = ((pd.to_numeric(df['Adj Close'].iloc[1:]) - pd.to_numeric(df['Open'].iloc[1:]))
@@ -23,4 +22,12 @@ annual_return = (100 * (pd.to_numeric(df["Adj Close"].iloc[-1]) - pd.to_numeric(
                  pd.to_numeric(df["Adj Close"].iloc[1]))
 
 # Calculate volatility, where volatility = std_dev * sqrt(trading period)
-volatility = df["Daily Returns"].std() * math.sqrt(252)
+# 252 represents the 252 trading days within a year
+n_time_periods = len(df)
+volatility = df["Daily Returns"].std() * math.sqrt(n_time_periods)
+
+# Calculate Maximum Draw-down
+peak_index = df["High"][1:].idxmax()
+peak = pd.to_numeric(df["High"][peak_index])
+trough = pd.to_numeric(df["Low"][peak_index:].min())
+maximum_draw_down = (peak - trough) / peak
