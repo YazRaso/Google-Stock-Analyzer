b0f6acdc276db9b4831d5e0c1ecf60fc1fcad599
YazRaso <daedricrend@gmail.com>
2024-12-22 12:07:29 +0400

    Added Maximum Drawdown

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 # Import packages
 import pandas as pd
 import numpy as np
-import streamlit as st
+import math
 
 # Use Kaggle dataset
-stock_data = pd.read_csv("./GOOG.csv")
+stock_data = pd.read_csv('./GOOG.csv')
 
 # Initialize dataframe
 df = pd.DataFrame(stock_data)
 
-# Clean data
+# TODO: Clean data to represent 252 trading days in a year
 df.dropna()
 df.drop_duplicates()
 
-df['price change %'] = ((pd.to_numeric(df['Adj Close'].iloc[1:]) - pd.to_numeric(df['Open'].iloc[1:]))
-                        / pd.to_numeric(df['Open'].iloc[1:])) * 100
+# Calculate Daily returns
+df['Daily Returns'] = ((pd.to_numeric(df['Adj Close'].iloc[1:]) - pd.to_numeric(df['Open'].iloc[1:]))
+                        / pd.to_numeric(df['Open'].iloc[1:]))
+df['Daily Returns %'] = df['Daily Returns'] * 100
 
-# Creating UI using streamlit
-st.write("Hello World!")
+# Calculate Annual Return (2024-01-02) - (2024-12-03)
+annual_return = (100 * (pd.to_numeric(df["Adj Close"].iloc[-1]) - pd.to_numeric(df["Adj Close"].iloc[1])) /
+                 pd.to_numeric(df["Adj Close"].iloc[1]))
+
+# Calculate volatility, where volatility = std_dev * sqrt(trading period)
+volatility = df["Daily Returns"].std() * math.sqrt(252)
