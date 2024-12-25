6a271b5778445dd3a760d27d6836996204c1f198
YazRaso <seyedyazdanrasoulzadeh@gmail.com>
2024-12-25 14:57:13 +0400

    Finished up UI and tidied up main script. Merry Chirstmas
    '

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 import math
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import streamlit as st
 
 # Use Kaggle dataset
 stock_data = pd.read_csv("./GOOG.csv")
@@ -23,20 +25,24 @@ df = df.dropna()
 df = df.drop_duplicates()
 
 # Calculate daily returns
-df['Daily Returns'] = ((df['Adj Close']- df['Open']) / df['Open'])
+df['Daily Returns'] = ((df['Adj Close'] - df['Open']) / df['Open'])
 df['Daily Returns %'] = df["Daily Returns"] * 100
 
 # Calculate Annual Return (2024-01-02) - (2024-12-03)
 annual_return = 100 * (df["Adj Close"].iloc[-1] - df["Adj Close"].iloc[0]) / df["Adj Close"].iloc[0]
 
 # Calculate volatility, where volatility = std_dev * sqrt(trading period)
-annual_volatility = df["Daily Returns"].std() * math.sqrt(len(df))
+annual_volatility = round((df["Daily Returns"].std() * math.sqrt(len(df))), 3)
 
 peak_index = df["High"].idxmax()
-peak = pd.to_numeric(df["High"][peak_index])
-trough = pd.to_numeric(df["Low"][peak_index:].min())
-max_draw_down = (peak - trough) / peak
-print(max_draw_down)
+peak = df["High"].iloc[peak_index]
+trough_index = df["Low"].iloc[peak_index:].idxmin()
+trough = df["Low"].iloc[trough_index]
+max_draw_down = round(((peak - trough) / peak), 3)
+
+trough_price = round(df['Close'].iloc[trough_index], 1)
+peak_price = round(df['Close'].iloc[peak_index], 1)
+
 
 # Create visuals
 
@@ -44,97 +50,107 @@ print(max_draw_down)
 columns = ['Volume', 'Daily Returns', 'Adj Close',
            'Open', 'Close']
 
+
 # Calculate Correlation Matrix
-correlation_matrix = df[columns].corr()
+def corr_matrix():
+    correlation_matrix = df[columns].corr()
 
-# Plot Heatmap
-plt.figure(figsize=(10, 8))
-sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
-plt.title("Correlation Heatmap of Google Stock (2024)")
-plt.show()
+    # Plot Heatmap
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+    plt.title("Correlation Heatmap of Google Stock (2024)")
+    plt.show()
+    st.pyplot(plt)
 
-# Plot the Closing Price
-plt.figure(figsize=(12, 6))
-plt.plot(df['Price'], df['Close'], label='Closing Price', color='green')
+    st.title("A correlation matrix ")
 
-# Add labels and title
-plt.title("Google Stock Closing Prices (2024)", fontsize=16)
-plt.xlabel("Date", fontsize=12)
-plt.ylabel("Price (USD)", fontsize=12)
-plt.legend()
-plt.grid(True)
-plt.show()
-
-import plotly.graph_objects as go
 
 # Calculate SMAs (Simple Moving Average)
 df['50 SMA'] = df['Close'].rolling(50).mean()
 df['200 SMA'] = df['Close'].rolling(200).mean()
 
-# Create the candlestick chart
-candle_stick_fig = go.Figure(data=[
-    go.Candlestick(
-        x=df.index,
-        open=df['Open'],
-        high=df['High'],
-        low=df['Low'],
-        close=df['Close'],
-        increasing_line_color='green',
-        decreasing_line_color='red'
+
+def cand_stick():
+    # Create the candlestick chart
+    candle_stick_fig = go.Figure(data=[
+        go.Candlestick(
+            x=df['Price'],
+            open=df['Open'],
+            high=df['High'],
+            low=df['Low'],
+            close=df['Close'],
+            increasing_line_color='green',
+            decreasing_line_color='red'
+        )
+    ])
+
+    # Add the 50-day moving average to the chart
+    candle_stick_fig.add_trace(
+        go.Scatter(
+            x=df['Price'],  # Same x-axis (Date/Time)
+            y=df['50 SMA'],
+            mode='lines',
+            name='50-Day SMA',
+            line=dict(color='orange', width=1.5)
+        )
     )
-])
 
-# Add the 50-day moving average to the chart
-candle_stick_fig.add_trace(
-    go.Scatter(
-        x=df.index,  # Same x-axis (Date/Time)
-        y=df['50 SMA'],
-        mode='lines',
-        name='50-Day SMA',
-        line=dict(color='orange', width=1.5)
+    # Add the 200-day moving average to the chart
+    candle_stick_fig.add_trace(
+        go.Scatter(
+            x=df['Price'],
+            y=df['200 SMA'],
+            mode='lines',
+            name='200-Day SMA',
+            line=dict(color='green', width=1.5)
+        )
     )
-)
 
-# Add the 200-day moving average to the chart
-candle_stick_fig.add_trace(
-    go.Scatter(
-        x=df.index,
-        y=df['200 SMA'],
-        mode='lines',
-        name='200-Day SMA',
-        line=dict(color='green', width=1.5)
+    # Update layout for the chart
+    candle_stick_fig.update_layout(
+        title='Google Stock Candlestick Chart 2024',
+        xaxis_title='Date',
+        yaxis_title='Price (USD)',
+        xaxis_rangeslider_visible=False,
     )
-)
 
-# Update layout for the chart
-candle_stick_fig.update_layout(
-    title='Google Stock Candlestick Chart 2024',
-    xaxis_title='Date',
-    yaxis_title='Price (USD)',
-    xaxis_rangeslider_visible=False,
-)
+    # Display the chart
+    st.plotly_chart(candle_stick_fig, use_container_width=True)
+    st.title("Outline")
 
-# Display the chart
-candle_stick_fig.show()
+    st.write(
+        "The above chart represents Alphabet's stock from the start of the year up to the 3rd of December. The chart includes detailed candlestick patterns with additional 50-Day and 200-Day SMAs (Simple Moving Averages) "
+        "A SMA is designed to show the mean closing prices of a stock in a specified time period, in other words, the average price within a time frame.")
 
-fig, ax1 = plt.subplots(figsize=(10, 6))
+    st.title("Analysis")
+    st.write(
+        f"A quick glance on Alphabet's stock appears to peak on {df['Price'].iloc[peak_index]} with a price of {peak_price} USD, "
+        f"consequently the stocks 2024 Maximum Draw Down seems to be approximately {max_draw_down} with the trough on"
+        f" {df["Price"].iloc[trough_index]} with a price of {trough_price}USD. Alphabet's stock in 2024 has a volatility of {annual_volatility} opposed to the S&P 500's volatility of 0.1795. Alphabet's annual return in 2024 was {round(annual_return, 3)}% similar to the S&P 500's return of 24%.")
 
-# Plot the stock price (line chart)
-ax1.plot(df['Price'], df['Close'], color='blue', label='Closing Price')
-ax1.set_xlabel('Date')  # Label reflects the datetime on x-axis
-ax1.set_ylabel('Price (USD)', color='blue')
-ax1.tick_params(axis='y', labelcolor='blue')
 
-# Create a secondary axis for volume
-ax2 = ax1.twinx()
-ax2.bar(df['Price'], df['Volume'], color='green', alpha=0.3, label='Volume')
-ax2.set_ylabel('Volume', color='green')
-ax2.tick_params(axis='y', labelcolor='green')
+def volume_traded():
+    fig, ax1 = plt.subplots(figsize=(10, 6))
 
-# Add a title and legend
-fig.suptitle('Google Stock: Price vs. Volume', fontsize=14)
-ax1.legend(loc='upper left')
-ax2.legend(loc='upper right')
+    # Plot the stock price (line chart)
+    ax1.plot(df['Price'], df['Close'], color='blue', label='Closing Price')
+    ax1.set_xlabel('Date')  # Label reflects the datetime on x-axis
+    ax1.set_ylabel('Price (USD)', color='blue')
+    ax1.tick_params(axis='y', labelcolor='blue')
 
-plt.show()
+    # Create a secondary axis for volume
+    ax2 = ax1.twinx()
+    ax2.bar(df['Price'], df['Volume'], color='green', alpha=0.3, label='Volume')
+    ax2.set_ylabel('Volume', color='green')
+    ax2.tick_params(axis='y', labelcolor='green')
 
+    # Add a title and legend
+    fig.suptitle('Google Stock: Price vs. Volume', fontsize=14)
+    ax1.legend(loc='upper left')
+    ax2.legend(loc='upper right')
+
+    st.pyplot(fig)
+
+    st.title("Outline")
+    st.write("The above chart represents Alphabet's stock price by its volume traded. Price by Volume charts are used by traders in the context of technical analysis to predict resistance and support of a specific security.")
+    st.title("Analysis")
--- ui.py
+++ ui.py
@@ -1,5 +1,17 @@
 import streamlit as st
-import main
+from main import *
+
 # Creating UI using streamlit
-st.write("Hello World!")
-choice = st.selectbox("Quarterly performance", ("Q1", "Q2", "Q3", "Q4"))
+st.sidebar.title("About📌")
+st.sidebar.write("""Hello! 😊👋🏻 I am Yazdan Rasoulzadeh, a software and finance enthusiast pursuing a Bachelors in both Computer Science and Business Administration. With Q4 coming to an end, I decided to build this passion project in order to deepen my understanding of stock terminology 📊 but also working with data 📈, especially within the context of turning data into meaningful visuals.  
+
+All the code for this project can be found [here](https://github.com/YazRaso) at my GitHub repo 💻, my LinkedIn can be found [here](https://ca.linkedin.com/in/yazdan-rasoulzadeh-a77a3b309) 🌐.  
+
+I look forward to connecting with you! 🤝 This is not in any shape or form financial advice. 🚫💸""")
+
+st.title("On Alphabet's Stock in 2024: A Detailed Report 📈")
+
+st.write(f"2024 has been a relatively strong year for Alphabet's stock (Google) "
+         "as we approach the end of Q4, this report has been designed for a visual representation of the stocks performance")
+
+cand_stick()
